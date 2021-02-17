85db8c63409485b4e2861331fa23869ccfc50b52
basne <basne@LAPTOP-P81H9CQ0>
2021-02-17 13:49:32 -0600

    lab06

--- apps/lab02app/lab02app/main.swift
+++ apps/lab02app/lab02app/main.swift
@@ -1,72 +1,29 @@
-//
-//  main.swift
-//  lab02app
-//
-//  Created by ASU Math CS MAC 3 on 1/20/21.
-//
 
-import Foundation
-
+//ios
+//App
+//lab05absolute
+// storyboard
+// UIKit App Delegate
+// inside viewController.swift inside a class
 
 
-enum Nucleotide: Character, Comparable {
-    case A="A", C="C", G="G", T="T"}
-    
+class ViewController: UIViewController{
+
+	override func viewDidLoad(){
+	super.viewDidLoad()
+	let fristFrame = CGRect(x: 160, y: 240, width: 100, height: 150)
+	let firstView = UIview(frame: fristFrame)
+	firstView.backgroundColor = UIColor.blue
+	view.addSubview(firstView)
+	
+	let secondFrame = CGRect (x : 20 , y : 30 , width : 50 , height : 50)
+	let secondView = UIView ( frame : secondFrame )
+	secondView . backgroundColor = UIColor . green
+	view.addSubview (secondView)
+
+	
+		}
+
+	}
 
 
-func  <(lhs: Nucleotide, rhs:Nucleotide) -> Bool {
-    return lhs.rawValue < rhs.rawValue
-    
-}
- 
-
-
-
-
-typealias Codon = (Nucleotide, Nucleotide, Nucleotide)
-typealias Gene = [Codon]
-
-/*
-func linearContains (_ array: Gene, item: Codon) -> Bool{
-    for element in array where item == element{
-        return true
-    }
-    return false
-}
- comment
-*/
- 
-func binaryContains(_ array: Gene, item: Codon) -> Bool {
- var low = 0
-    var high = array.count - 1
-    while low <= high {
-        let mid = (low + high) / 2
-        if array[mid] < item {
-            low = mid + 1
-        } else if array[mid] > item {
-            high = mid - 1
-        }else{
-            return true
-        }
-    }
-        return false
-    }
-
-
-
-func stringToGene (_ s:String) -> Gene {
-    var gene = Gene()
-    for i in stride(from: 0, to: s.count, by:3) {
-        guard (i+2) < s.count else {return gene}
-        
-        if let n1 = Nucleotide.init(rawValue: s[s.index(s.startIndex, offsetBy: i)]),
-           
-        let n2 = Nucleotide.init(rawValue: s[s.index(s.startIndex, offsetBy: i + 1)]),
-           
-        let n3 = Nucleotide.init(rawValue: s[s.index(s.startIndex, offsetBy: i + 2)]) {
-                    gene.append((n1,n2,n3))
-                                     }
-                                    
-    }
-    return gene
-}
