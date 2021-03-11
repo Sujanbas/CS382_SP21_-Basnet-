d000ae63777a42a9a6d6dc40ef1c7958972455a3
basne <basne@LAPTOP-P81H9CQ0>
2021-03-11 14:42:58 -0600

    Asg 02

---
+++ apps/Sysinfo Asg 02/Sysinfo Asg 02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sysinfo Asg 02
+//
+//  Created by user191359 on 3/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
